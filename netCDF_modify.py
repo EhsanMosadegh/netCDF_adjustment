bc9e488df24826d538506a431dcdc9cb903fbc96
Ehsan <ehsan@Ehsans-MacBook-Pro.local>
2018-12-08 21:03:32 -0800

    updated file name and path

--- netCDF_modify.py
+++ netCDF_modify.py
@@ -18,10 +18,14 @@ output_dir = work_dir+'/outputs'
 
 ########################################
 
-file_name = 'filename'
+file_name = 'BCON_v521_test_for_v53_profile'
 
-file_path = 'filepath'
+file_path = '/Users/ehsan/Documents/PYTHON_CODES/netCDF_modify/inputs/'
 
-file_name_path = file_name+file_path
+file_name_path = file_path+file_name
 
-nc_input_file = Datasetset(file_name_path , 'r')
+print( '-> file name is "%s" ' %file_name )
+print( '-> file path is "%s"' %file_path )
+print( '-> file name and path is "%s"' %file_name_path )
+
+nc_input_file = Dataset(file_name_path , 'r')
