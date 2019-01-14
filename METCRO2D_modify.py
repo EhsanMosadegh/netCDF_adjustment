f664e4ee55155c8e6c61bb105a5a961a36b9001d
Ehsan <ehsan@Ehsans-MacBook-Pro.local>
2019-01-14 15:59:27 -0800

    added and modified pathes

--- METCRO2D_modify.py
+++ METCRO2D_modify.py
@@ -2,34 +2,35 @@
 
 ########################################
 # created on: Jan 14, 2019
-# 
+#
 # author: ehsanm (ehsanm@dri.edu)
 # Purpose: to manipulate METCDRO2D values
 ########################################
 
-from NetCDF4 import Dataset
+from netCDF4 import Dataset
 import numpy as np
+import os
 
 ########################################
 
-work_dir = '/Users/ehsan/Documents/PYTHON_CODES/netCDF_modify'
+work_dir = '/Users/ehsan/Documents/Python_projects/netCDF_modify'
 repository_name = 'netCDF_adjustment'
-script_dir = work_dir+'/github/'+repository_name
-input_dir = work_dir+'/inputs/METCRO2D_inputs'
-output_dir = work_dir+'/outputs/METCRO2D_output'
+script_dir = work_dir+'github/'+repository_name
+input_dir = work_dir+'inputs/METCRO2D_inputs/'
+output_dir = work_dir+'outputs/METCRO2D_output'
 
 ########################################
 
 favorite_value = 0.001
 nc_variable = 'LAI'
 
-input_file_name = 'METCRO2D_160711'
-input_file_full_path = input_dir+input_file_name
+input_file_name = 'METCRO2D_160711.nc'
+input_file_full_path = os.path.join( input_dir , input_file_name)
 
 # read the netcdf file
-nc_file = Dataset( input_file_full_path , 'r+')
+nc_file = Dataset( input_file_full_path ,'r+')
 
-if ( nc_file.nc_variables[nc_variable] == 0 ) :
+if ( nc_file.nc_variables[ nc_variable ] == 0 ) :
 
 	print( '-> there are zeros in %s' %(input_file_name) )
 	print( '-> we will replace zero values with %s' %(favorite_value) )
