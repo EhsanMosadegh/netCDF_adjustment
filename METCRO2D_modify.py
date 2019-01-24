7b3989eba656d04740ab8a8921a56e69089ca228
Ehsan <ehsan@Ehsans-MacBook-Pro.local>
2019-01-23 18:39:11 -0800

    added a big chunk of code defined functions

--- METCRO2D_modify.py
+++ METCRO2D_modify.py
@@ -12,6 +12,71 @@ import numpy as np
 import os
 
 ########################################
+# function
+
+def metcro2d_modify ( met_file_full_path ):
+
+        print('-> copy the netcdf file')
+
+        # read the copied netcdf file
+        nc_file = Dataset( met_file_full_path ,'r+')
+
+        LAI_VAR = nc_file.variables[ nc_variable ]
+
+        print('-> LAI dimensions are = ' , LAI_VAR.dimensions )
+        print('-> size of each dim is = ' , LAI_VAR.shape )
+
+        tstep_Ubound = LAI_VAR.shape[0]
+        lay_Ubound = LAI_VAR.shape[1]
+        row_Ubound = LAI_VAR.shape[2]
+        col_Ubound = LAI_VAR.shape[3]
+
+
+        for itstep in range(0,tstep_Ubound,1):
+
+            for ilay in range(0,lay_Ubound,1):
+
+                for irow in range(0,row_Ubound,1):
+
+                    for icol in range(0,col_Ubound,1):
+
+                        if ( LAI_VAR[ itstep , ilay , irow , icol ] == 0 ):
+
+                            print( '-> there are zero valus at TSTEP=%s, LAY=%s, ROW=%s, COL=%s, and we will replace zero values with %s' %(itstep,ilay,irow,icol,favorite_value) )
+
+                            LAI_VAR[ itstep , ilay , irow , icol ] = favorite_value
+
+                        #else:
+
+                            	#print( '-> there was not any zero inside %s' %(input_file_name) )
+        nc_file.close()
+
+########################################
+# function
+# QA to check if there is still zero:
+# select a subset of LAY array
+
+def metcro2d_QA (met_file_full_path):
+
+        print('-> doing the QA to check if there is any zero left...')
+
+        nc_file = Dataset( met_file_full_path ,'r')
+
+        LAI_VAR = nc_file.variables[ nc_variable ]
+
+        LAI_array = np.array( LAI_VAR[:,:,:,:] )
+
+        if ( np.amin(LAI_array) == 0 ) :
+
+        	print( '-> ERROR: there are still zero valus in %s' %(input_file_name) )
+
+        else:
+
+        	print( '-> no zero value found inside %s' %(input_file_name) )
+
+        nc_file.close()
+
+########################################
 # set paths
 
 work_dir = '/Users/ehsan/Documents/Python_projects/netCDF_modify'
@@ -35,6 +100,40 @@ for imonth in month_list :
 
     if ( imonth == '07' or imonth == '08' or imonth == '10' ) :
 
+        for iday in range(1,32) :
+
+            if ( iday <= 9 ) :
+
+                iday = '0'+str(iday)
+
+                #print('iday is %s' %iday)
+
+            else:
+
+                iday = str(iday)
+
+            date_tag = yr+imonth+iday
+
+            input_file_name = 'METCRO2D_'+date_tag+'.nc'
+
+            print('-> input file is %s' %input_file_name)
+
+            input_file_full_path = os.path.join( input_dir , input_file_name)
+
+            if ( os.path.isfile(input_file_full_path) == False ):
+
+                print('-> %s not available, go to the next file' %(input_file_name) )
+                continue
+
+            else:
+                print('-> %s exists, and will be modified' %( input_file_name) )
+                #metcro2d_modify( input_file_full_path)
+                #metcro2d_QA ( input_file_full_path)
+
+
+
+    elif ( imonth == '09' or imonth == '11') :
+
         for iday in range(1,31) :
 
             if ( iday <= 9 ) :
@@ -47,93 +146,26 @@ for imonth in month_list :
 
             date_tag = yr+imonth+iday
 
-    elif ( imonth == '09' or imonth == '11') :
+            input_file_name = 'METCRO2D_'+date_tag+'.nc'
 
-        for iday in range(1,30) :
+            print('-> input file is %s' %input_file_name)
 
-            if ( iday <= 9 ) :
+            input_file_full_path = os.path.join( input_dir , input_file_name)
 
-                iday = '0'+str(iday)
+            if ( os.path.isfile(input_file_full_path) == False ):
+
+                print('-> %s not available, go to the next file' %(input_file_name) )
+                continue
 
             else:
-
-                iday = str(iday)
-
-            date_tag = yr+imonth+iday
+                print('-> %s exists, and will be modified' %( input_file_name) )
+                #metcro2d_modify( input_file_full_path)
+                #metcro2d_QA ( input_file_full_path)
 
     else:
 
         print('-> month not used, check month number')
 
-
-            print('-> copy METCRO2D file...')
-
+print('***** SUCCESSFULL FINISH *****')
 
 
-
-
-
-
-
-
-
-
-
-            input_file_name = 'METCRO2D_160711.nc'
-            input_file_full_path = os.path.join( input_dir , input_file_name)
-
-# read the netcdf file
-nc_file = Dataset( input_file_full_path ,'r+')
-
-LAI_VAR = nc_file.variables[ nc_variable ]
-
-print('-> LAI dimensions are = ' , LAI_VAR.dimensions )
-print('-> size of each dim is = ' , LAI_VAR.shape )
-
-tstep_Ubound = LAI_VAR.shape[0]
-lay_Ubound = LAI_VAR.shape[1]
-row_Ubound = LAI_VAR.shape[2]
-col_Ubound = LAI_VAR.shape[3]
-
-
-for itstep in range(0,tstep_Ubound,1):
-
-    for ilay in range(0,lay_Ubound,1):
-
-        for irow in range(0,row_Ubound,1):
-
-            for icol in range(0,col_Ubound,1):
-
-                if ( LAI_VAR[ itstep , ilay , irow , icol ] == 0 ):
-
-                    print( '-> there are zero valus at TSTEP=%s, LAY=%s, ROW=%s, COL=%s, and we will replace zero values with %s' %(itstep,ilay,irow,icol,favorite_value) )
-
-                    LAI_VAR[ itstep , ilay , irow , icol ] = favorite_value
-
-                #else:
-
-                    	#print( '-> there was not any zero inside %s' %(input_file_name) )
-nc_file.close()
-
-########################################
-# QA to check if there is still zero:
-
-# select a subset of LAY array
-
-print('-> doing the QA to check if there is any zero left...')
-
-nc_file = Dataset( input_file_full_path ,'r')
-
-LAI_VAR = nc_file.variables[ nc_variable ]
-
-LAI_array = np.array( LAI_VAR[:,:,:,:] )
-
-if ( np.amin(LAI_array) == 0 ) :
-
-	print( '-> ERROR: there are still zero valus in %s' %(input_file_name) )
-
-else:
-
-	print( '-> no zero value found inside %s' %(input_file_name) )
-
-nc_file.close()
